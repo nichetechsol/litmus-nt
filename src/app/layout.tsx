373ffb0d5c4bd8f9d217dd8be886e09d7cac31e1
Parth Roka <parthr@nichetech.in>
2024-06-03 11:38:39 +0530

    fix(add): build issue fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,25 @@
-import { Metadata } from 'next';
-import * as React from 'react';
+'use client';
+import { usePathname } from 'next/navigation';
 
-import '@/styles/globals.css';
 import '@/styles/globals.scss';
-// !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
-import '@/styles/colors.css';
 
-import { siteConfig } from '@/constant/config';
+// Import your layout components
+// import ContentLayout from '@/shared/layout-components/layout/content-layout';
 
-// !STARTERCONF Change these default meta
-// !STARTERCONF Look at @/constant/config to change them
-export const metadata: Metadata = {
-  metadataBase: new URL(siteConfig.url),
-  title: {
-    default: siteConfig.title,
-    template: `%s | ${siteConfig.title}`,
-  },
-  description: siteConfig.description,
-  robots: { index: true, follow: true },
-  // !STARTERCONF this is the default favicon, you can generate your own from https://realfavicongenerator.net/
-  // ! copy to /favicon folder
-  icons: {
-    icon: '/favicon/favicon.ico',
-    shortcut: '/favicon/favicon-16x16.png',
-    apple: '/favicon/apple-touch-icon.png',
-  },
-  manifest: `/favicon/site.webmanifest`,
-  openGraph: {
-    url: siteConfig.url,
-    title: siteConfig.title,
-    description: siteConfig.description,
-    siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.jpg`],
-    type: 'website',
-    locale: 'en_US',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.title,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/images/og.jpg`],
-    // creator: '@th_clarence',
-  },
-  // authors: [
-  //   {
-  //     name: 'Theodorus Clarence',
-  //     url: 'https://theodorusclarence.com',
-  //   },
-  // ],
+// export const metadata = {
+//   title: 'My Next.js App', // Add your desired metadata here
+// };
+
+// Define interface for layout functions
+interface LayoutComponent {
+  (props: { children: React.ReactNode }): JSX.Element;
+}
+
+// Define layout mapping using an interface
+const layouts: { [route: string]: LayoutComponent } = {
+  // '/dashboard': ContentLayout,
+  // '/organization': ContentLayout,
+  // '/orgdashboard': ContentLayout,
 };
 
 export default function RootLayout({
@@ -55,9 +27,27 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const locationPath = usePathname();
+
+  // Determine the appropri  console.log(locationPath);ate layout based on the route
+  const baseRoute = getBaseRoute(locationPath || '/auth');
+  const Layout = layouts[baseRoute]; // Use ContentLayout as default
+
   return (
-    <html>
-      <body>{children}</body>
+    <html lang='en'>
+      <head>
+        <title>Litmus</title>
+        {/* Add additional meta tags as needed */}
+      </head>
+      <body>
+        {Layout && <Layout>{children}</Layout>}{' '}
+        {/* Conditionally render layout */}
+      </body>
     </html>
   );
 }
+
+const getBaseRoute = (path: string) => {
+  const parts = path.split('/');
+  return `/${parts[1]}`;
+};
