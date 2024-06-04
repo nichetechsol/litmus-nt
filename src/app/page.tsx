e971f7ecd932cd09e8689426a81229517e0ca545
NiraliBT <nirali@nichetech.in>
2024-06-04 15:57:29 +0530

    fix(add): icon fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ const LoginForm = () => {
   const [emailError, setEmailError] = useState('');
   const [password, setPassword] = useState('');
   const [passwordErr, setPasswordErr] = useState('');
-  // const [passwordshow1, setpasswordshow1] = useState(false);
+  const [passwordshow1, setpasswordshow1] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
   const [loading, setLoading] = useState<boolean>(false);
   const navigate = useRouter();
@@ -249,7 +249,7 @@ const LoginForm = () => {
                           <div className='input-group'>
                             <input
                               name='password'
-                              type='password'
+                              type={passwordshow1 ? 'text' : 'password'}
                               value={password}
                               onChange={handlePasswordChange}
                               onKeyDown={handleKeyPress}
@@ -258,6 +258,21 @@ const LoginForm = () => {
                               id='signin-password'
                               placeholder='password'
                             />
+                            <button
+                              onClick={() => setpasswordshow1(!passwordshow1)}
+                              aria-label='button'
+                              className='ti-btn ti-btn-light !rounded-s-none !mb-0'
+                              type='button'
+                              id='button-addon2'
+                            >
+                              <i
+                                className={`${
+                                  passwordshow1
+                                    ? 'ri-eye-line'
+                                    : 'ri-eye-off-line'
+                                } align-middle`}
+                              ></i>
+                            </button>
                           </div>
                           {passwordErr && (
                             <div className='text-danger'>{passwordErr}</div>
