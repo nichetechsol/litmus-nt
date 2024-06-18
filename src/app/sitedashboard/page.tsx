a2ca4f4ca980e4acaee29d3b04c5635c8925d803
NehalNT <nehal@nichetech.in>
2024-06-18 18:08:59 +0530

    fix(add): solution page

--- src/app/sitedashboard/page.tsx
+++ src/app/sitedashboard/page.tsx
@@ -285,7 +285,7 @@ const Page = () => {
         const data: any = await getSiteUserRole(user_id, site_id);
 
         if (data) {
-          setuserrole3(data.data.name);
+          setuserrole3(data.data.id);
         } else {
           // console.log("No Role Found.");
         }
