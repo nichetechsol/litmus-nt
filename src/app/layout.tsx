615888b3e67d2661d6990184f26510d01d688b74
AbusufiyanNT <abusufiyan@nichetech.in>
2024-06-28 11:00:45 +0530

    design bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,8 +85,12 @@ export default function RootLayout({
   const Layout = layouts[baseRoute] || AuthenticationLayout; // Use ContentLayout as default
   return (
     <html lang='en'>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+
       <head>
         <title>Litmus</title>
+
         {/* Add additional meta tags as needed */}
       </head>
       <body>
