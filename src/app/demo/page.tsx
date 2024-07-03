87a9f00749dada8d3404a84e9c00da746b5727e2
soumiyabhandari <soumiya@nichetech.in>
2024-07-03 16:34:24 +0530

    fix(add):update organization

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -21,15 +21,15 @@ const OrgDashboard = () => {
         //   lastname: 'Roka',
         // };
 
-        // const data1: any = {
-        //   userName: 'shruti@nichetech.com',
-        //   siteName: 'Nichetech Ahmedabad',
-        //   orgName: 'Nichetech',
-        //   token:
-        //     'eyJhbGciOiJIUzI1NiIsImtpZCI6ImpmZVZXUEovY3RVdElDRTYiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzE5OTAwODgxLCJpYXQiOjE3MTk4OTcyODEsImlzcyI6Imh0dHBzOi8vZW1zaml1enRjaW5oYXBhdXJjcmwuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6ImQ2MTA4ODVmLWU2Y2YtNDdmZi04ODBhLTkxN2YzN2Q2Y2EzOSIsImVtYWlsIjoicGFydGhyQG5pY2hldGVjaC5jb20iLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIiwicHJvdmlkZXJzIjpbImVtYWlsIl19LCJ1c2VyX21ldGFkYXRhIjp7fSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJwYXNzd29yZCIsInRpbWVzdGFtcCI6MTcxOTg5NzI3OX1dLCJzZXNzaW9uX2lkIjoiY2ExN2MxMDktZGJiNi00YjM1LWI2Y2EtNzkwZjE0YTMyZDdkIiwiaXNfYW5vbnltb3VzIjpmYWxzZX0.fqElYLq12BA1h6f77UC6bLV64o4IfRTpE5whkOa4dxo',
-        // };
+        const data1: any = {
+          userName: 'shruti@nichetech.com',
+          siteName: 'Nichetech Ahmedabad',
+          orgName: 'Nichetech',
+          token:
+            'eyJhbGciOiJIUzI1NiIsImtpZCI6ImpmZVZXUEovY3RVdElDRTYiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzE5OTAwODgxLCJpYXQiOjE3MTk4OTcyODEsImlzcyI6Imh0dHBzOi8vZW1zaml1enRjaW5oYXBhdXJjcmwuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6ImQ2MTA4ODVmLWU2Y2YtNDdmZi04ODBhLTkxN2YzN2Q2Y2EzOSIsImVtYWlsIjoicGFydGhyQG5pY2hldGVjaC5jb20iLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIiwicHJvdmlkZXJzIjpbImVtYWlsIl19LCJ1c2VyX21ldGFkYXRhIjp7fSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJwYXNzd29yZCIsInRpbWVzdGFtcCI6MTcxOTg5NzI3OX1dLCJzZXNzaW9uX2lkIjoiY2ExN2MxMDktZGJiNi00YjM1LWI2Y2EtNzkwZjE0YTMyZDdkIiwiaXNfYW5vbnltb3VzIjpmYWxzZX0.fqElYLq12BA1h6f77UC6bLV64o4IfRTpE5whkOa4dxo',
+        };
 
-        const data: any = await deleteDomains(269, 201, 6);
+        const data: any = await deleteDomains(data1);
 
         // const { data, error } = await supabase
         //   .from('site_users')
