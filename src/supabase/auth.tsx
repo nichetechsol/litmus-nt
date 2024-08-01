e02d4426677699493f52e4d480c857b7b693d5a7
Harsh Patel <harsh@nichetech.in>
2024-08-01 12:36:06 +0530

    fix: redirect

--- src/supabase/auth.tsx
+++ src/supabase/auth.tsx
@@ -192,8 +192,6 @@ async function AsureAuth(provider: AuthProvider): Promise<void> {
       provider,
       options: {
         scopes: 'email',
-        redirectTo:
-          'https://litmus-nt-git-azure-nichetechabhays-projects.vercel.app/auth/v1/callback',
       },
     });
   } catch (error) {
