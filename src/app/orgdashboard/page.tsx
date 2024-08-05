e98b13be5455945da10959f44ef14b3e5a8d7979
KunalNT <kunal@nichetech.in>
2024-08-05 15:45:03 +0530

    bug 44 email invitation

--- src/app/orgdashboard/page.tsx
+++ src/app/orgdashboard/page.tsx
@@ -31,6 +31,7 @@ import { getUserRole } from '@/supabase/org_details';
 import {
   addUserToOrganization,
   getOrgUserRole,
+  inviteSendToUser,
   modifyUserOfOrganization,
   removeUserFromOrganization,
   searchUsers,
@@ -574,6 +575,44 @@ const OrgDashboard = () => {
             }
           } else {
             toast.error(result.data, { autoClose: 3000 });
+            if (result.data == 'User is not in Central V2') {
+              swal({
+                title: 'Error',
+                text: `${result.data} Do you want to invite the user?`,
+                icon: 'error',
+                buttons: ['No', 'Yes'],
+              }).then(async (willSendInvite) => {
+                if (willSendInvite) {
+                  setLoading(true);
+                  //   const modalTrigger: any = document.querySelector(
+                  //     "[data-hs-overlay='#todo-compose1']",
+                  //   );
+                  //   if (modalTrigger) {
+                  //     modalTrigger.click();
+                  // }
+                  const data = {
+                    email: email.toLowerCase(),
+                    token: onlyToken,
+                    userName: userEmail,
+                    orgName: orgName,
+                  };
+
+                  await refreshToken();
+
+                  const result = await inviteSendToUser(data);
+
+                  if (result.errorCode === 0) {
+                    setLoading(false);
+                    toast.success(result.message, { autoClose: 3000 });
+                    fetchData2();
+                    CountData();
+                  } else {
+                    toast.error(result.message, { autoClose: 3000 });
+                    setLoading(false);
+                  }
+                }
+              });
+            }
             const button = document.getElementById('close-modal-btn');
             if (button) {
               button.click();
@@ -738,6 +777,49 @@ const OrgDashboard = () => {
       setDataTOAutoFill2([]);
     }
   };
+
+  // // for inivatation
+  // const handelInvitation = async () => {
+  //   setLoading(true);
+  //   const isValid = await validateForm();
+  //   if (isValid) {
+  //     try {
+  //       // Prepare the data for the invitation
+  //       const data = {
+  //         email: email.toLowerCase(),
+  //         token: onlyToken,
+  //         userName: userEmail,
+  //         orgName: orgName,
+  //       };
+
+  //       await refreshToken();
+
+  //       const result = await inviteSendToUser(data);
+
+  //       if (result.errorCode === 0) {
+  //         toast.success(result.message, { autoClose: 3000 });
+  //         fetchData2();
+  //         CountData();
+  //       } else {
+  //         toast.error(result.message, { autoClose: 3000 });
+  //       }
+
+  //       // Close the modal
+  //       if (closeModalButtonRef.current) {
+  //         closeModalButtonRef.current.click();
+  //       }
+  //       closeModal();
+  //       roleChange();
+  //       setLoading(false);
+  //     } catch (error) {
+  //       closeModal();
+  //       setLoading(false);
+  //     }
+  //   } else {
+  //     closeModal();
+  //     setLoading(false);
+  //   }
+  // };
   return (
     <>
       {loading && <Loader />}
@@ -1254,6 +1336,134 @@ const OrgDashboard = () => {
                         </div>
                       </div>
                     </div>
+
+                    {/* this is another model */}
+                    {/* <div>
+                      <div
+                        id='todo-compose1'
+                        // className='hs-overlay hidden ti-modal'
+                        className='hs-overlay hidden ti-modal  [--overlay-backdrop:static]'
+                      >
+                        <div className='hs-overlay-open:mt-7 ti-modal-box mt-0 ease-out'>
+                          <div className='ti-modal-content'>
+                            <div className='ti-modal-header'>
+                              <h6
+                                className='modal-title text-[1rem] font-semibold'
+                                id='mail-ComposeLabel1'
+                              >
+                                Invite User
+                              </h6>
+                              <button
+                                type='button'
+                                className='hs-dropdown-toggle !text-[1rem] !font-semibold !text-defaulttextcolor'
+                                onClick={() => handleFiledClear()}
+                                data-hs-overlay='#todo-compose1'
+                                ref={closeModalButtonRef}
+                              >
+                                <span className='sr-only'>Close</span>
+                                <i className='ri-close-line'></i>
+                              </button>
+                            </div>
+                            <div className='ti-modal-body !overflow-visible px-4'>
+                              <div className='grid grid-cols-12 gap-2'>
+                                <div className='xl:col-span-12 col-span-12'>
+                                  <label
+                                    htmlFor='Email'
+                                    className='ti-form-label'
+                                  >
+                                    Email <span className='text-danger'>*</span>
+                                  </label>
+                                  <input
+                                    type='text'
+                                    autoComplete='none'
+                                    className={`form-control w-full ${
+                                      email ? '' : 'input-error'
+                                    } ${emailError ? 'input-error' : ''} `}
+                                    id='Email'
+                                    disabled={!changeFlage}
+                                    placeholder='Enter Email'
+                                    onChange={handleEmailChange}
+                                    onFocus={() => setIsFocusedOnEmail(true)}
+                                    onBlur={() => setIsFocusedOnEmail(false)}
+                                    // onKeyDown={handleKeyPress}
+                                    maxLength={320}
+                                    value={email}
+                                  />
+                                  {emailError && (
+                                    <div className='text-danger'>
+                                      {emailError}
+                                    </div>
+                                  )}{' '}
+                                </div>
+                                <div className='xl:col-span-12 col-span-12'>
+                                  <label
+                                    htmlFor='task-name'
+                                    className='ti-form-label'
+                                  >
+                                    Role <span className='text-danger'>*</span>
+                                  </label>
+                                  <select
+                                    style={{ cursor: 'pointer' }}
+                                    className={`form-select ${
+                                      role ? '' : 'input-error'
+                                    }
+                                        ${roleError ? 'input-error' : ''}
+                                        ${role === '' ? 'deselect-main' : ''}`}
+                                    onChange={handleRoleChange}
+                                    value={role}
+                                  >
+                                    <option value='' hidden>
+                                      Select a Role
+                                    </option>
+                                    {roles &&
+                                      roles.map((role) => (
+                                        <option key={role.id} value={role.id}>
+                                          {role.name}
+                                        </option>
+                                      ))}
+                                  </select>
+                                  {roleError && (
+                                    <div className='text-danger'>
+                                      {roleError}
+                                    </div>
+                                  )}
+                                </div>
+                              </div>
+                            </div>
+                            <div className='ti-modal-footer'>
+                              <button
+                                type='button'
+                                id='close-modal-btn'
+                                className='hs-dropdown-toggle ti-btn  ti-btn-light align-middle'
+                                data-hs-overlay='#todo-compose1'
+                                ref={closeModalButtonRef}
+                                onClick={() => handleFiledClear()}
+                              >
+                                Cancel
+                              </button>
+                              <button
+                                type='button'
+                                // className='ti-btn bg-primary text-white !font-medium'
+                                onClick={() => handelInvitation()}
+                                // disabled={!addbuttonclass2}
+                                // className={
+                                //   addbuttonclass2
+                                //     ? 'ti-btn bg-primary text-white !font-medium'
+                                //     : 'ti-btn bg-gray-500 text-white !font-medium'
+                                // }
+
+                                className='ti-btn bg-primary text-white !font-medium'
+                              >
+                                Send Invitation
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div> */}
+
+                    {/* end of the model */}
+
                     <div className='box-body'>
                       <div className='overflow-x-auto'>
                         <table className='table min-w-full whitespace-nowrap table-hover border table-bordered'>
