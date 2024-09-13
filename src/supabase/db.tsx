82803ae9bb669103941e0f537cd8b4339dee54c6
soumiyabhandari <soumiya@nichetech.in>
2024-09-13 14:34:46 +0530

    fix(add):push .env

--- src/supabase/db.tsx
+++ src/supabase/db.tsx
@@ -8,3 +8,17 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+// import { createClient } from '@supabase/supabase-js';
+
+// // Define the URL and key for Supabase, with fallback error handling
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+// if (!supabaseUrl || !supabaseKey) {
+//   throw new Error('Missing Supabase URL or Key');
+// }
+
+// // Create the Supabase client
+// export const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
